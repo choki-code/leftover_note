e7edf7ec3ce1fbbd81cb839724ea68db6c5ffce4
choki-code <923choki@gmail.com>
2026-09-23 02:17:23 +0900

    新規登録と編集で学校情報を入力できるようにし、退会を塞ぐ

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, skip: :registrations
+  # 退会（DELETE /users）だけ塞ぐ。ユーザーを消すと料理・献立・残食の記録まで消えるため
+  devise_scope :user do
+    resource :registration,
+            only: %i[new create edit update],
+            path: "users",
+            path_names: { new: "sign_up" },
+            controller: "devise/registrations",
+            as: :user_registration do
+      get :cancel
+    end
+  end
 
   root "dishes#index"
 
